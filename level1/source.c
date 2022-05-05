74450c3363a2e3c2c2dd074d557f25eddf887057
Adeline BENOIT <adbenoit@e3r9p18.clusters.42paris.fr>
2022-05-05 16:25:02 +0200

    restruct

---
+++ level1/source.c
@@ -0,0 +1,17 @@
+void run(void)
+
+{
+  fwrite("Good... Wait what?\n",1,0x13,stdout);
+  system("/bin/sh");
+  return;
+}
+
+void main(void)
+
+{
+  undefined local_50 [76];
+  
+  gets(local_50);
+  return;
+}
+
